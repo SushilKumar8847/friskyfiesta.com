9aeca5a053b3198929d360068a9d1ea6cde2b829
Sushilkumar8847 <skymodfista@gmail.com>
2022-05-26 13:01:10 +0530

    upload

---
+++ include/footer.php
@@ -0,0 +1,148 @@
+<footer class="xs-footer-section">
+		<div class="container">
+			<div class="footer-top-area">
+				<div class="row">
+					<div class="col-md-4">
+						<div class="footer-logo">
+							<a href="">
+								<img src="images/logo.png" alt="" width="60" height="65">
+							</a>
+						</div>
+					</div>
+					<div class="col-md-8">
+						<ul class="address-info-list list-inline">
+							<li>
+								<div class="address-icon">
+									<img src="assets/images/pin.png" alt="">
+								</div>
+								<div class="address-info"><a
+										href="https://goo.gl/maps/tmx9zUsPMQFcUMn39" target="blank">Noida
+										 <br> Uttar Pradesh, India</a></div>
+							</li>
+							<li>
+								<div class="address-icon"><img src="assets/images/massage.png" alt=""></div>
+								<div class="address-info"><a href="tel:+91 7985498635">(+91) 8279668975</a> ( Feel free to Call) <br>
+									<a
+										href="https://html.xpeedstudio.com/cdn-cgi/l/email-protection#20494e464f604558414d504c450e434f4d"><span
+											class="__cf_email__"
+											data-cfemail="670e09010827021f060a170b024904080a">Info@friskyfiesta.com</span></a>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="footer-main">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-lg-3">
+						<div class="footer-widget">
+							<h4 class="xs-content-title">Company</h4>
+							<ul class="xs-lsit">
+								<li><a href="about.html">About us</a></li>
+								<li><a href="#">Partners</a></li>
+								<li><a href="#">Case Studies</a></li>
+								<li><a href="#">Our Team</a></li>
+								<li><a href="#">Career</a></li>
+								<li><a href="contact.html">Contact</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-md-6 col-lg-3">
+						<div class="footer-widget">
+							<h4 class="xs-content-title">Services</h4>
+							<ul class="xs-lsit">
+								<li><a href="#">Digital Marketing</a></li>
+								<li><a href="#">Application Development</a></li>
+								<li><a href="#">Web Development</a></li>
+								<li><a href="#">Graphic Designing</a></li>
+								<li><a href="#">Video Editing</a></li>
+								<li><a href="#">Advertisement</a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-md-6 col-lg-3">
+						<div class="footer-widget">
+							<h4 class="xs-content-title">Articles</h4>
+							<ul class="articles-list">
+								<li>
+									<a class="entry-title" href="blog-single.html">Twice profit than before you ever</a>
+									<span class="entry-meta"><i class="icon icon-clock"></i> January 14, 2018</span>
+								</li>
+								<li>
+									<a class="entry-title" href="blog-single.html">Twice profit than before you ever</a>
+									<span class="entry-meta"><i class="icon icon-clock"></i> January 14, 2018</span>
+								</li>
+								<li>
+									<a class="entry-title" href="blog-single.html">Twice profit than before you ever</a>
+									<span class="entry-meta"><i class="icon icon-clock"></i> January 14, 2018</span>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-md-6 col-lg-3">
+						<div class="footer-widget">
+							<h4 class="xs-content-title">Request a quote</h4>
+							<form action="#" class="contact-form" method="post">
+								<input type="email" name="email" placeholder="Email" class="form-control">
+								<textarea name="message" class="form-control" placeholder="Message" cols="30"
+									rows="10"></textarea>
+								<input type="submit" value="Submit now" class="submit-btn">
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="partner-area-wraper">
+			<div class="container">
+				<div class="partner-area">
+					<div class="row">
+						<div class="col-md-8">
+							<ul class="xs-lsit list-inline">
+								<li class="title">PARTNERSHIP :</li>
+								<li><img src="assets/images/partner/partner-1.png" alt=""></li>
+								<li><img src="assets/images/partner/partner-2.png" alt=""></li>
+								<li><img src="assets/images/partner/partner-3.png" alt=""></li>
+								<li><img src="assets/images/partner/partner-4.png" alt=""></li>
+							</ul>
+						</div>
+						<div class="col-md-4">
+							<ul class="xs-list list-inline text-right">
+								<li><img src="assets/images/card/visa.png" alt=""></li>
+								<li><img src="assets/images/card/master-card.png" alt=""></li>
+								<li><img src="assets/images/card/discover.png" alt=""></li>
+								<li><img src="assets/images/card/american-express.png" alt=""></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="copyright">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="copyright-text">
+							<p>Copyright &copy; 2018, All Right Reserved <a
+									href="https://themeforest.net/user/xpeedstudio/portfolio">SEOPress</a></p>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<ul class="social-list">
+							<li><a href="https://www.facebook.com/" class="facebook"><i class="fa fa-facebook"></i></a>
+							</li>
+							<li><a href="https://twitter.com/" class="twitter"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="https://bd.linkedin.com/" class="linkedin"><i class="fa fa-linkedin"></i></a>
+							</li>
+							<li><a href="https://www.instagram.com/" class="instagram"><i
+										class="fa fa-instagram"></i></a></li>
+							<li><a href="https://plus.google.com/discover" class="googlePlus"><i
+										class="fa fa-google-plus"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
